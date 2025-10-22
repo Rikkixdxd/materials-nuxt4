fd46f1cca41799fdee233efe3623bb7c940660ce
Rikkixdxd <alexs.tkach@mail.ru>
2025-10-22 20:13:52 +0700

    feat(header): Обновлен хедер и стили

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,13 +1,49 @@
 <template>
-    <div>
-
-    </div>
+    <header class="app-header">
+        <UiContainer>
+            <NuxtLink to="/" class="app-header__logo"><Logo /></NuxtLink>
+            <div class="app-header__actions">
+                <UiButton><span>Создать материал</span><Add /></UiButton>
+            </div>
+        </UiContainer>
+    </header>
 </template>
 
 <script setup lang="ts">
-
+import Logo from '~/assets/svgs/Logo.svg'
+import Add from '~/assets/svgs/Add.svg'
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+.app-header
+    padding: 20px 0
+    background-color: $color-surface
+    @media(max-width: 768px)
+        padding: 8px 0
+
+    .ui-container
+        display: flex
+        justify-content: space-between
+        align-items: center
+
+    &__logo
+        display: inline-flex
+        
+    .ui-button 
+        @media(max-width: 768px)
+            padding: 0
+            width: 48px
+            height: 48px
+            display: flex
+            justify-content: center
+            align-items: center
+        svg
+            display: none
+            font-size: 48px
+            @media(max-width: 768px)
+                display: block
+        span
+            @media(max-width: 768px)
+                display: none
 
 </style>

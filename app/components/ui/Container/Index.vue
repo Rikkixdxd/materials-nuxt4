9ef3bb64216b9bca6f70532fdb768407ecff0b1e
Rikkixdxd <alexs.tkach@mail.ru>
2025-10-23 00:38:51 +0700

    feat(ui): Добавлен ui компонент Container

---
+++ app/components/ui/Container/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="ui-container">
+        <slot />
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+.ui-container 
+    max-width: 1224px
+    margin: 0 auto
+    padding: 0 16px
+
+</style>

9a8dc91a2d7b09dfd9a884d96c4a0307c9026e86
Rikkixdxd <alexs.tkach@mail.ru>
2025-10-23 00:34:30 +0700

    feat(types): Добавлен новый тип Material

---
+++ app/types/Material.ts
@@ -0,0 +1,9 @@
+
+export type Material = {
+    id: number
+    title: string
+    short_description: string
+    datetime: string
+    description_html: string
+    description_json: Record<string, any>
+}

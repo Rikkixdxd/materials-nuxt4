437730dac25446177a07f0474f65f25fd01e8a07
Rikkixdxd <alexs.tkach@mail.ru>
2025-10-23 20:02:24 +0700

    feat(api): Обновлена оболочка для работы с апи

--- app/composables/ApiClient.ts
+++ app/composables/ApiClient.ts
@@ -4,16 +4,25 @@ export const ApiClient = () => {
     const config = useRuntimeConfig()
     const baseUrl = config.public.apiBaseUrl
     const token = config.public.token
+    const commonHeaders = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
     
     const GET = async <K extends keyof ApiPaths['GET']>(path: K, id?: number): Promise<ApiPaths['GET'][K]> => {
         let requestUrl = path as string;
         requestUrl = requestUrl.replace('{alias}', token);
         
-        if (id && requestUrl.includes('{id}')) {
+        if (id && requestUrl.includes('id}')) {
+            // Можно сделать через URLSearchParams
             requestUrl = requestUrl.replace('{id}', id.toString());
         }
-        const response = await fetch(`${baseUrl}${requestUrl}`)
-        return response.json()
+
+        const response = await fetch(`${baseUrl}${requestUrl}`, {
+            method: 'GET',
+            headers: commonHeaders
+        })
+        return await response.json();
     }
 
     const POST = async <K extends keyof ApiPaths['POST']>(path: K, data: ApiPaths['POST'][K]): Promise<any> => {
@@ -22,9 +31,7 @@ export const ApiClient = () => {
 
         const response = await fetch(`${baseUrl}${requestUrl}`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: commonHeaders,
             body: JSON.stringify(data)
         })
 

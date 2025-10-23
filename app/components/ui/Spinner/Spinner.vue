4597836a84f628ac0942e287f5866167f1b2c270
Rikkixdxd <alexs.tkach@mail.ru>
2025-10-23 20:01:25 +0700

    feat(ui): Добавлен ui компонент загрузки

---
+++ app/components/ui/Spinner/Spinner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<style scoped>
+.spinner {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 4px solid transparent;
+  border-radius: 50%;
+  border-top-color: #3b82f6;
+  animation: spin 0.8s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
